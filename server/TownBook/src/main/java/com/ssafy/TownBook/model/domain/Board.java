c0da00a5be929d548fcb747a1132bcfc3842aeb4
곽승엽 <ekd7822@naver.com>
2023-01-17 13:34:53 +0900

    DOMAINUPDATE

--- server/TownBook/src/main/java/com/ssafy/TownBook/model/domain/Board.java
+++ server/TownBook/src/main/java/com/ssafy/TownBook/model/domain/Board.java
@@ -1,45 +1,74 @@
 package com.ssafy.TownBook.model.domain;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@ToString
 @Entity
 public class Board {
 
     @Id
-    Long no;
+    @Column(name = "board_no")
+    private Long boardNo;
 
     @NotNull
-    String title;
+    @Column(name = "board_title")
+    private String boardTitle;
 
     @NotNull
-    String category;
+    @Column(name = "board_category")
+    private String boardCategory;
 
     @NotNull
+    @Column(name = "board_content")
     @Lob
-    String text;
+    private String boardContent;
 
-    LocalDateTime writeDate;
+    @NotNull
+    @Column(name = "board_write_date")
+    private LocalDateTime boardWriteDate;
 
-    @Column(columnDefinition = "Integer default 0")
-    Integer views;
+    @NotNull
+    @Column(name = "board_views", columnDefinition = "Integer default 0")
+    private Integer boardViews;
 
-//    @OneToMany(mappedBy = "board")
-//    List<Hit> hit = new ArrayList<>();
+    @OneToMany(mappedBy = "board")
+    List<Hit> hit = new ArrayList<>();
 
-//    @OneToMany(mappedBy = "board")
-//    List<Comment> comment = new ArrayList<>();
+    @OneToMany(mappedBy = "board")
+    List<Comment> comment = new ArrayList<>();
 
     @ManyToOne
     Account account;
 
+    @Builder
+    public Board(Long boardNo, String boardTitle, String boardCategory, String boardContent,
+            LocalDateTime boardWriteDate, Integer boardViews, List<Hit> hit, List<Comment> comment,
+            Account account) {
+        this.boardNo = boardNo;
+        this.boardTitle = boardTitle;
+        this.boardCategory = boardCategory;
+        this.boardContent = boardContent;
+        this.boardWriteDate = boardWriteDate;
+        this.boardViews = boardViews;
+        this.hit = hit;
+        this.comment = comment;
+        this.account = account;
+    }
 }
