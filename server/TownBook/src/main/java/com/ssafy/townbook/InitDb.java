1eac8d14d1a2c23c10701e18dcb2b6163613751d
HyunseokCheong <hyunseokcheong@gmail.com>
2023-01-25 12:09:05 +0900

    0125 정현석 dbinit

---
+++ server/TownBook/src/main/java/com/ssafy/townbook/InitDb.java
@@ -0,0 +1,60 @@
+package com.ssafy.townbook;
+
+import com.ssafy.townbook.model.entity.Book;
+import java.time.LocalDate;
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 프로젝트 실행하면 책 DB에 입력
+ */
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+    
+    private final InitService initService;
+    
+    @PostConstruct
+    public void init() {
+        initService.dbInit();
+    }
+    
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        
+        private final EntityManager em;
+        
+        public void dbInit() {
+            Book book1 = createBook("8984993751", "8", "토지", 0, "박경리", "커뮤니케이션 북스", LocalDate.now(),
+                    null, null);
+            em.persist(book1);
+            
+            Book book2 = createBook("9788960777330", "0",
+                    "자바 ORM 표준 JPA 프로그래밍(스프링 데이터 예제 프로젝트로 배우는 전자정부 표준 데이터베이스 프레임워크)",
+                    0, "김영한", "에이콘출판", LocalDate.now(),
+                    "https://www.nl.go.kr/seoji/fu/ecip/dbfiles/CIP_FILES_TBL/4721454_5.txt"
+                    , "https://www.nl.go.kr/seoji/fu/ecip/dbfiles/CIP_FILES_TBL/4721454_3.jpg");
+            em.persist(book2);
+        }
+        
+        private Book createBook(String bookIsbn, String bookSubject, String bookTitle, int bookVol, String bookAuthor,
+                String bookPublisher, LocalDate bookPublishPredate, String bookIntroductionURL, String bookTitleURL) {
+            Book book = new Book();
+            book.setBookIsbn(bookIsbn);
+            book.setBookSubject(bookSubject);
+            book.setBookTitle(bookTitle);
+            book.setBookVol(bookVol);
+            book.setBookAuthor(bookAuthor);
+            book.setBookPublisher(bookPublisher);
+            book.setBookPublishPredate(bookPublishPredate);
+            book.setBookIntroductionURL(bookIntroductionURL);
+            book.setBookTitleURL(bookTitleURL);
+            return book;
+        }
+    }
+}
