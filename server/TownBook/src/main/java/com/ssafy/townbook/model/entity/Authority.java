06de227023bb5d85a5d6f1470f510970208c4f83
HyunseokCheong <hyunseokcheong@gmail.com>
2023-02-01 10:55:19 +0900

    0201 정현석 git rm test

--- server/TownBook/src/main/java/com/ssafy/townbook/model/entity/Authority.java
+++ server/TownBook/src/main/java/com/ssafy/townbook/model/entity/Authority.java
@@ -3,8 +3,6 @@ package com.ssafy.townbook.model.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,13 +11,15 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "authority")
 public class Authority {
-
+    
     @Id
     @Column(name = "authority_name")
     private String authorityName;
+    
+    @Builder
+    public Authority(String authorityName) {
+        this.authorityName = authorityName;
+    }
 }
