c33c6e9d4f1df3a2d46c8235db845ec909e31b56
HyunseokCheong <hyunseokcheong@gmail.com>
2023-02-01 12:29:31 +0900

    0201 정현석 rm

--- server/TownBook/src/main/java/com/ssafy/townbook/model/repository/BookLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.townbook.model.repository;
-
-import com.ssafy.townbook.model.entity.BookLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookLogRepository extends JpaRepository<BookLog, Long> {
-    
-    BookLog findBookLogByBookLogNo(Long bookLogNo);
-}
