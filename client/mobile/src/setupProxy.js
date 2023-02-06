928ac4ee611ad2d01c0e187b830ded1be9a120b2
yeong <ap0674@naver.com>
2023-02-06 17:03:39 +0900

    0206 이영재 https

--- client/mobile/src/setupProxy.js
+++ client/mobile/src/setupProxy.js
@@ -15,7 +15,7 @@ module.exports = function(app){
 
   app.use(
     createProxyMiddleware('/server', {
-      target: 'http://i8b201.p.ssafy.io:8081/backend',
+      target: 'https://i8b201.p.ssafy.io:8081/backend',
       pathRewrite: {
         '^/server':''
       },
