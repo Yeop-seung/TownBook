bf87e1b4e961515ae1fbd48dac2d6e43a7620fca
곽승엽 <ekd7822@naver.com>
2023-02-09 15:54:07 +0900

    update

--- server/TownBook/src/main/java/com/ssafy/townbook/controller/FileController.java
+++ server/TownBook/src/main/java/com/ssafy/townbook/controller/FileController.java
@@ -4,6 +4,7 @@ import com.ssafy.townbook.model.dto.request.SaveFileRequestDto;
 import com.ssafy.townbook.model.dto.response.SaveOneResponseDto;
 import com.ssafy.townbook.model.service.FileService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -34,14 +35,14 @@ public class FileController {
 
     /**
      * 회원번호로 파일 찾기
-     * 
+     *
      * @param accountNo
      * @return
      * @throws Exception
      */
     @Transactional
     @GetMapping("/{accountNo}")
-    public ResponseEntity<SaveOneResponseDto> findFileByAccountNo(@PathVariable Long accountNo) throws Exception{
-        return new ResponseEntity<SaveOneResponseDto>(fileService.findFileByAccountNo(accountNo),HttpStatus.OK);
+    public ResponseEntity<SaveOneResponseDto> findFileByAccountNo(@PathVariable Long accountNo) throws Exception {
+        return new ResponseEntity<SaveOneResponseDto>(fileService.findFileByAccountNo(accountNo), HttpStatus.OK);
     }
 }
