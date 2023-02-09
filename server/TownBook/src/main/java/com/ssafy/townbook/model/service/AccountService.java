df86f4686d9b2403d052460b43a3704f32dc3d36
HyunseokCheong <hyunseokcheong@gmail.com>
2023-02-09 10:07:01 +0900

    0209 정현석 account response dto

--- server/TownBook/src/main/java/com/ssafy/townbook/model/service/AccountService.java
+++ server/TownBook/src/main/java/com/ssafy/townbook/model/service/AccountService.java
@@ -22,7 +22,7 @@ public interface AccountService {
     Boolean updatePassword(String accountEmail, String tmpPassword);
     
     JSONArray findAccountBookCnt(Long accountNo) throws Exception;
-
+    
     AccountDto findAccountByAccountEmail(String accountEmail);
-
+    
 }
