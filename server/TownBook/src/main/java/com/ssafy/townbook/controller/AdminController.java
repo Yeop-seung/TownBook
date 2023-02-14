55db981bbf2b02f323317cebd60665f59889e962
곽승엽 <ekd7822@naver.com>
2023-02-14 09:47:39 +0900

    0214 곽승ì  Point Type User

--- server/TownBook/src/main/java/com/ssafy/townbook/controller/AdminController.java
+++ server/TownBook/src/main/java/com/ssafy/townbook/controller/AdminController.java
@@ -7,6 +7,7 @@ import com.ssafy.townbook.model.service.MyPageServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,8 +56,20 @@ public class AdminController {
      * @param accountNo
      * @return Integer
      */
-    @GetMapping("/givePoint/{accountNo}/{point}")
+    @GetMapping("/giveOne/{accountNo}/{point}")
     public ResponseEntity<FindOneResponseDto> givePointOneUser(@PathVariable Long accountNo, @PathVariable Integer point){
         return new ResponseEntity<FindOneResponseDto>(adminService.givePointOneUser(accountNo,point),HttpStatus.OK);
+    }
+
+    /**
+     * Type이 1인 유저 : 취약계층 전체 회원에게 포인트 부여
+     *
+     * @param point
+     * @return Boolean
+     */
+    @Transactional
+    @GetMapping("/giveType/{point}")
+    public ResponseEntity<FindOneResponseDto> givePointTypeUser(@PathVariable Integer point){
+        return new ResponseEntity<FindOneResponseDto>(adminService.givePointTypeUser(point),HttpStatus.OK);
     }
 }
--- server/TownBook/src/main/java/com/ssafy/townbook/model/repository/AdminRepository.java
+++ server/TownBook/src/main/java/com/ssafy/townbook/model/repository/AdminRepository.java
@@ -1,11 +1,15 @@
 package com.ssafy.townbook.model.repository;
 
 import com.ssafy.townbook.model.entity.Account;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Account, Long> {
-    
+
     /**
      * 회원번호로 단일 회원을 조회
      *
@@ -13,4 +17,16 @@ public interface AdminRepository extends JpaRepository<Account, Long> {
      * @return Optional<Account>
      */
     Optional<Account> findAccountByAccountNo(Long accountNo);
+
+    /**
+     * Type이 1인 유저 : 취약계층 전체 회원에게 포인트 부여
+     *
+     * @param point
+     * @param type
+     * @return Boolean
+     */
+    @Modifying
+    @Query("update Account m set m.accountPoint = m.accountPoint + :point where m.accountType = :type and m.accountActivated = true")
+    int givePointTypeUser(@Param("point") Integer point, @Param("type") Integer type);
 }
+
