174ff461ea020380072f00e89e0693065b6a62a2
HyunseokCheong <hyunseokcheong@gmail.com>
2023-04-09 11:33:20 +0900

    230409_BE_CHEONG_MOID_RemoveDuplicateFile

--- server/TownBook/src/main/java/com/ssafy/townbook/model/repository/AuthorityRepository 2.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.townbook.model.repository;
-
-import com.ssafy.townbook.model.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-
-}
